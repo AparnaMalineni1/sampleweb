76d02b3410969b092bff7e36127df2564f819f67
AparnaMalineni1 <malineniaparna1@gmail.com>
2024-06-25 16:02:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("clickMeButton");
+
+    button.addEventListener("click", function() {
+        alert("Button was clicked!");
+    });
+});
